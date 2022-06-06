8cb4feeae5610d53e48431370c8c1f2537bf83b3
Luis Cabral <luiscabral@Luiss-MacBook-Pro.local>
2022-06-06 13:38:22 -0500

    Resolver conflictos en cards

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -7,27 +7,6 @@
           <p>Bussiness model</p>
         </div>
 
-<<<<<<< Updated upstream
-        <div class="row" data-aos="fade-left">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="box" data-aos="zoom-in" data-aos-delay="100">
-              <h3>Free</h3>
-              <h4><sup>$</sup>0<span> / month</span></h4>
-              <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li class="na">Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-
-=======
         <div style="justify-content: center;" class="row" data-aos="fade-left">
           <div class="col-lg-3 col-md-6 mt-4 mt-md-0 ">
             <div class="box" data-aos="zoom-in" data-aos-delay="100">
@@ -45,7 +24,6 @@
               </div>
             </div>
           </div>
->>>>>>> Stashed changes
           <div class="col-lg-3 col-md-6 mt-4 mt-md-0">
             <div class="box featured" data-aos="zoom-in" data-aos-delay="200">
               <h3> Document authentification</h3>
@@ -62,44 +40,6 @@
             </div>
           </div>
 
-<<<<<<< Updated upstream
-          <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
-            <div class="box" data-aos="zoom-in" data-aos-delay="300">
-              <h3>Developer</h3>
-              <h4><sup>$</sup>29<span> / month</span></h4>
-              <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li>Massa ultricies mi</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-6 mt-4 mt-lg-0">
-            <div class="box" data-aos="zoom-in" data-aos-delay="400">
-              <span class="advanced">Advanced</span>
-              <h3>Ultimate</h3>
-              <h4><sup>$</sup>49<span> / month</span></h4>
-              <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li>Massa ultricies mi</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-
-=======
->>>>>>> Stashed changes
         </div>
 
 
