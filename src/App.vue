1d925717764cb6eab07478b00e1c9c12af7b35a1
xochescam-unipoli <99689677+xochescam-unipoli@users.noreply.github.com>
2022-06-06 01:33:33 -0500

    Descartar features

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
     <main>
       <about-component/>
-      <features-component/>
       <counts-component/>
       <details-component/>
       <team-component/>
@@ -21,7 +20,6 @@
 import HeaderComponent from './components/Header.vue'
 import HeroComponent from './components/Hero.vue'
 import AboutComponent from './components/About.vue'
-import FeaturesComponent from './components/Features.vue'
 import CountsComponent from './components/Counts.vue'
 import DetailsComponent from './components/Details.vue'
 import TestimonialsComponent from './components/Testimonials.vue'
@@ -39,7 +37,6 @@ export default {
     HeaderComponent, 
     HeroComponent,
     AboutComponent,
-    FeaturesComponent,
     CountsComponent,
     DetailsComponent,
     TestimonialsComponent,
