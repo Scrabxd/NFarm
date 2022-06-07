20689c48d50421bc9920b8bd82872bd1ec791541
xochescam-unipoli <99689677+xochescam-unipoli@users.noreply.github.com>
2022-06-07 10:48:26 -0500

    Marketplace

--- src/views/MarketplacePage.vue
+++ src/views/MarketplacePage.vue
@@ -6,8 +6,272 @@
           <p>Marketplace</p>
         </div>
 
+        <div class="container">
+          <div class="row justify-content-start">
+            <div class="col-3">
+
+              <h4 class="mb-3">Filters</h4>
+
+              
+
+              <div class="mb-4"> 
+                <p><b>Cattle breed</b></p>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Belga
+                  </label>
+                </div>
+                
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Hangus 
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Hereford
+                  </label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Limousine
+                  </label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Holstein
+                  </label>
+                </div>
+              </div>
+
+              <div class="mb-4"> 
+                <p><b>Age</b></p>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Calf  <small class="">(6 - 12 months)</small>
+                  </label>
+                </div>
+                
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Yearling <small class="">(12 - 24 months)  </small> 
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Steer   <small class="">(24 - 48 months)</small>
+                  </label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Cebón  <small class="">(24 - 48 months)</small>
+                  </label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Senior cattle <small class="">(48 months - older)</small>
+                  </label>
+                </div>
+              </div>
 
 
+
+
+              <div class="mb-4"> 
+                <p><b>Location</b></p>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Argentina
+                  </label>
+                </div>
+                
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Brazil 
+                  </label>
+                </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    México
+                  </label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    United States
+                  </label>
+                </div>
+
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                  <label class="form-check-label" for="flexCheckChecked">
+                    Australia
+                  </label>
+                </div>
+              </div>
+
+              <div class="mb-4"> 
+                <p><b>Weight</b></p>
+
+                <div class="row">
+                  <div class="col-6 input-group-sm">
+                      <input type="num" class="form-control input-sm" placeholder="min" aria-label="Username">
+
+                  </div>
+                  <div class="col-6 input-group-sm">
+                      <input type="num" class="form-control" placeholder="max" aria-label="Username">
+                  </div>
+                </div>
+              </div>
+
+              
+
+
+            </div>
+            <div class="col-9">
+                <div class="mb-5">
+                    <input type="text" class="form-control" placeholder="Search" aria-label="Username">
+                </div>
+
+              <div class="row">
+
+                <div class="col-4">
+                  <div class="card" style="width: 15rem;">
+                    <img src="/assets/img/items/1.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text d-flex justify-content-between">
+                        <b>Cordera</b>
+                        
+                        <b class="color-primary">
+                          600 USD
+                        </b>
+                      </p>
+                      <small> Holstein</small> <br>
+                      <small><i class="bi bi-geo-alt"></i> México</small>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-4">
+                  <div class="card" style="width: 15rem;">
+                    <img src="/assets/img/items/2.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text d-flex justify-content-between">
+                        <b>Lucera</b>
+                        
+                        <b class="color-primary">
+                          500 USD
+                        </b>
+                      </p>
+                      <small> Belga</small> <br>
+                      <small><i class="bi bi-geo-alt"></i> Australia</small>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-4">
+                  <div class="card" style="width: 15rem;">
+                    <img src="/assets/img/items/3.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text d-flex justify-content-between">
+                        <b>Gallarda</b>
+                        
+                        <b class="color-primary">
+                          800 USD
+                        </b>
+                      </p>
+                      <small> Holstein</small> <br>
+                      <small><i class="bi bi-geo-alt"></i> México</small>
+
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="row mt-5">
+
+                <div class="col-4">
+                  <div class="card" style="width: 15rem;">
+                    <img src="/assets/img/items/4.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text d-flex justify-content-between">
+                        <b>Romera</b>
+                        
+                        <b class="color-primary">
+                          600 USD
+                        </b>
+                      </p>
+                      <small> Hereford</small> <br>
+                      <small><i class="bi bi-geo-alt"></i> Brazil</small>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-4">
+                  <div class="card" style="width: 15rem;">
+                    <img src="/assets/img/items/5.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text d-flex justify-content-between">
+                        <b>Rubia</b>
+                        
+                        <b class="color-primary">
+                          900 USD
+                        </b>
+                      </p>
+                      <small> Belga</small> <br>
+                      <small><i class="bi bi-geo-alt"></i> México</small>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-4">
+                  <div class="card" style="width: 15rem;">
+                    <img src="/assets/img/items/6.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text d-flex justify-content-between">
+                        <b>Mora</b>
+                        
+                        <b class="color-primary">
+                          800 USD
+                        </b>
+                      </p>
+                      <small> Hereford</small> <br>
+                      <small><i class="bi bi-geo-alt"></i> Argentina</small>
+
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
 
       </div>
     </section>
